74a6ae255c3e68e2c61426a2bda5c6b92d2fdaae
Karim Machlab <karim@vegascity.org>
2023-12-08 17:37:13 +0000

    updated classroom configs

--- CommunityVenue/src/index.ts
+++ CommunityVenue/src/index.ts
@@ -5,12 +5,12 @@ import { setupUi } from './ui'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
 import { DisplayPanel } from './displayPanel'
 import { Podium } from './podium/podium'
-import { ClassroomManager } from '@dclu/dclu-liveteach/src/classroom'
+import { ClassroomManager, ControllerUI } from '@dclu/dclu-liveteach/src/classroom'
 import { PeerToPeerChannel } from "@dclu/dclu-liveteach/src/classroom/comms/peerToPeerChannel";
 import * as classroom1Config from "./classroomConfigs/classroom1Config.json"
 import * as classroom2Config from "./classroomConfigs/classroom2Config.json"
 import * as classroom3Config from "./classroomConfigs/classroom3Config.json"
-import * as classroom4Config from "./classroomConfigs/classroom3Config.json"
+import * as classroom4Config from "./classroomConfigs/classroom4Config.json"
 import { DoorManager } from './doors/doorManager'
 import { movePlayerTo } from '~system/RestrictedActions'
 import { SeatingData } from './SeatingData'
@@ -24,51 +24,56 @@ export function main() {
     })
     setupUi()
 
-    new Building() 
+    new Building()
     new LiftManager()
     new DoorManager()
 
     const communicationChannel = new PeerToPeerChannel()
     ClassroomManager.Initialise(communicationChannel, null, null, true)
 
+    // Add contract guid here for testing
+    ClassroomManager.SetTestContractGuid("c73c16d2-e2a7-4acc-bdda-fb2205b5d634")
+
     // Add wallet address here for testing
-    //ClassroomManager.AddTestTeacherAddress("0x0ced09e3728f7682e0fedf50f5fe1fe2e59add7b")
+    ClassroomManager.AddTestTeacherAddress("0x416fc23ffd33ded496650289e3c0d5b1ebd0033e")
 
-    //////////// Class 1 ////////////
+    //////////// Class 1 - Lecture Theatre 1 (left) ////////////
     ClassroomManager.RegisterClassroom(classroom1Config)
-    const podium1 = new Podium(Vector3.create(16,6.9,30.3), Vector3.create(0,-90,0))
+    const podium1 = new Podium(Vector3.create(16, 6.9, 30.3), Vector3.create(0, -90, 0))
     addScreen(Vector3.create(0.35, 1.7, -0.06), Quaternion.fromEulerDegrees(45, 90, 0), Vector3.create(0.2, 0.2, 0.2), podium1.entity)
-    addScreen(Vector3.create(11.9,7,45.58), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(8.15,8.15,8.15),null)
+    addScreen(Vector3.create(11.9, 7, 45.58), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(8.15, 8.15, 8.15), null)
 
-    //////////// Class 2 ////////////
+    //////////// Class 2 - Lecture Theatre 2 (right) ////////////
     ClassroomManager.RegisterClassroom(classroom2Config)
-    const podium2 = new Podium(Vector3.create(33,6.9,30.3), Vector3.create(0,-90,0))
+    const podium2 = new Podium(Vector3.create(33, 6.9, 30.3), Vector3.create(0, -90, 0))
     addScreen(Vector3.create(0.35, 1.7, -0.06), Quaternion.fromEulerDegrees(45, 90, 0), Vector3.create(0.2, 0.2, 0.2), podium2.entity)
-    addScreen(Vector3.create(36.9,7,45.58), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(8.15,8.15,8.15),null)
+    addScreen(Vector3.create(36.9, 7, 45.58), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(8.15, 8.15, 8.15), null)
 
-    //////////// Class 3 ////////////
+    //////////// Class 3 - Classroom 1 (bottom) ////////////
     ClassroomManager.RegisterClassroom(classroom3Config)
-    const podium3 = new Podium(Vector3.create(32,6.9,16.8), Vector3.create(0,90,0))
+    const podium3 = new Podium(Vector3.create(32, 6.9, 16.8), Vector3.create(0, 90, 0))
     addScreen(Vector3.create(0.35, 1.7, -0.06), Quaternion.fromEulerDegrees(45, 90, 0), Vector3.create(0.2, 0.2, 0.2), podium3.entity)
-    addScreen(Vector3.create(39.15,9.73,20.5), Quaternion.fromEulerDegrees(0,0,0), Vector3.create(4.1,4.1,4.1),null)
+    addScreen(Vector3.create(39.15, 9.73, 20.5), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(4.1, 4.1, 4.1), null)
 
-    //////////// Class 4 ////////////
+    //////////// Class 4 - Classroom 2 (top) ////////////
     ClassroomManager.RegisterClassroom(classroom4Config)
-    const podium4 = new Podium(Vector3.create(32,6.9+6.1,16.8), Vector3.create(0,90,0))
+    const podium4 = new Podium(Vector3.create(32, 6.9 + 6.1, 16.8), Vector3.create(0, 90, 0))
     addScreen(Vector3.create(0.35, 1.7, -0.06), Quaternion.fromEulerDegrees(45, 90, 0), Vector3.create(0.2, 0.2, 0.2), podium4.entity)
-    addScreen(Vector3.create(39.15,9.73+6.1,20.5), Quaternion.fromEulerDegrees(0,0,0), Vector3.create(4.1,4.1,4.1),null)
-    
+    addScreen(Vector3.create(39.15, 9.73 + 6.1, 20.5), Quaternion.fromEulerDegrees(0, 0, 0), Vector3.create(4.1, 4.1, 4.1), null)
+
     // Add seating 
-     let seatingData: SeatingData = new SeatingData()
+    let seatingData: SeatingData = new SeatingData()
 
     // Apply offset
     let offset = Vector3.create(0, 0.2, 0)
-    seatingData.seats.forEach((seat,index) => {
+    seatingData.seats.forEach((seat, index) => {
         // update ids after combining
         seat.id = index
         seat.position = Vector3.add(seat.position, offset)
-        seat.lookAtTarget = Vector3.create(seat.position.x,seat.position.y,seat.position.z+5)
+        seat.lookAtTarget = Vector3.create(seat.position.x, seat.position.y, seat.position.z + 5)
     });
+
+    //ControllerUI.Show()
 
     //Debugging  
     // seatingData.seats.forEach(seat => {
@@ -82,4 +87,4 @@ export function main() {
 
 export function addScreen(_position: Vector3, _rotation: Quaternion, _scale: Vector3, _parent: ecs.Entity): void {
     ClassroomManager.AddScreen(_position, _rotation, _scale, _parent)
-  }
+}
